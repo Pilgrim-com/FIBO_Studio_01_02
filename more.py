5163934a67568157c1376cfd614be6caae4f2641
Suphawit Kaewpan <106130274+Pilgrim-com@users.noreply.github.com>
2024-05-25 06:47:24 +0700

    delete class canvas in more

--- more.py
+++ more.py
@@ -13,13 +13,6 @@ class Variable:
     ORIGIN = (160, 400)
     G = 9.81
 
-class canvas(Frame):
-    def __init__(self, parent, width, height, bg):
-        super().__init__(parent, bg = '#FFFFFF')
-        
-        # Create and place canvas for triangle
-        self.canvas = Canvas(parent, width = width, height = height, bg = bg)
-
 class Title(Frame):
     def __init__(self, parent, title):
         super().__init__(parent, bg = '#2C2C2C')
